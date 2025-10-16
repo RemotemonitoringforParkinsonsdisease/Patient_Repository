53c9ebcc5a8309f49336d0b9385ea6d2143007d9
dvr992003 <dvr992003@gmail.com>
2025-10-16 15:13:30 +0200

    Test David

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("Hello, World!");
         System.out.println("TONTO");
         System.out.println("Porfavor ya");
-        //Prueba David
+       //Hola (David)
     }
 }
