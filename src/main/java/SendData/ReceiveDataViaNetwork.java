1cb29a3c4563f77ad2416b0d59e15d6469c1ef99
AlexArecha <alex.arechabaletaguerrero@usp.ceu.es>
2025-11-06 19:16:50 +0100

    Send/Receive data via network

---
+++ src/main/java/SendData/ReceiveDataViaNetwork.java
@@ -0,0 +1,31 @@
+package SendData;
+
+public class ReceiveDataViaNetwork {
+    /*
+        private static void releaseResources(DataInputStream dataInputStream, InputStream inputStream,
+            Socket socket, ServerSocket socketServidor) {
+
+        try {
+            dataInputStream.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ReceiveBinaryDataViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            inputStream.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ReceiveBinaryDataViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ReceiveBinaryDataViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            socketServidor.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ReceiveBinaryDataViaNetwork.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+     */
+}
