93975c95f070f771cb7939b5c63dcd25f0c45c55
dvr992003 <dvr992003@gmail.com>
2025-11-15 13:19:30 +0100

    Constructores de report, hay que hacer Receive de Report

--- src/main/java/POJOS/Report.java
+++ src/main/java/POJOS/Report.java
@@ -7,49 +7,103 @@ import java.util.Random;
 import java.util.Set;
 
 public class Report {
-    private static List<String> idList = new ArrayList<String>();
     private String reportId;
     private Patient patient;
     private LocalDate reportDate;
     private String patientObservation;//El texto que le manda el paciente al doctor
-    private String doctorObservation;
     private Set<Symptoms> symptoms; //Sintomas de una lista cerrada
     private Set<Signal> signals; //La señal grabada por el bitalino del paciente, supongo que seran varios canales, puede ser un Set / List de String, hay que mirar tipo de datos del Bitalino
+    private String doctorObservation;
 
 
-    public Report(Patient patient, LocalDate reportDate, String patientObservation, String doctorObservation, Set<Symptoms> symptoms, Set<Signal> signals) {
+    public Report(Patient patient, LocalDate reportDate, String patientObservation, Set<Symptoms> symptoms, Set<Signal> signals,  String doctorObservation) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.patientObservation = patientObservation;
+        this.symptoms = symptoms;
+        this.signals = signals;
+        this.doctorObservation = doctorObservation;
+    }
+
+    public Report(Patient patient, LocalDate reportDate, String patientObservation, Set<Symptoms> symptoms, String doctorObservation) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.patientObservation = patientObservation;
+        this.symptoms = symptoms;
+        this.doctorObservation = doctorObservation;
+
+    }
+    public Report(Patient patient, LocalDate reportDate, Set<Symptoms> symptoms, Set<Signal> signals, String doctorObservation) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.symptoms = symptoms;
+        this.signals = signals;
+        this.doctorObservation = doctorObservation;
+    }
+    public Report(Patient patient, LocalDate reportDate, String patientObservation, Set<Signal> signals, String doctorObservation) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.patientObservation = patientObservation;
+        this.signals = signals;
+        this.doctorObservation = doctorObservation;
+    }
+    public Report(Patient patient, LocalDate reportDate, String patientObservation, String doctorObservation) {
         this.patient = patient;
         this.reportDate = reportDate;
         this.patientObservation = patientObservation;
         this.doctorObservation = doctorObservation;
-        this.symptoms = symptoms;
-        this.signals = signals;
     }
-
+    public Report(Patient patient, LocalDate reportDate, Set<Signal> signals, String doctorObservation) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.signals = signals;
+        this.doctorObservation = doctorObservation;
+    }
+    public Report(Patient patient, LocalDate reportDate, Set<Symptoms> symptoms, String doctorObservation) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.symptoms = symptoms;
+        this.doctorObservation = doctorObservation;
+    }
     public Report(Patient patient, LocalDate reportDate, String patientObservation, Set<Symptoms> symptoms, Set<Signal> signals) {
         this.patient = patient;
         this.reportDate = reportDate;
         this.patientObservation = patientObservation;
-        this.doctorObservation = doctorObservation;
-        this.symptoms = symptoms;
-        this.signals = signals;
-    }
-    public Report(Patient patient, LocalDate reportDate, String patientObservation, String doctorObservation, Set<Symptoms> symptoms) {
-        this.patient = patient;
-        this.reportDate = reportDate;
-        this.patientObservation = patientObservation;
-        this.doctorObservation = doctorObservation;
-        this.symptoms = symptoms;
-        this.signals = signals;
-    }
-    public Report(Patient patient, LocalDate reportDate, String doctorObservation, Set<Symptoms> symptoms, Set<Signal> signals) {
-        this.patient = patient;
-        this.reportDate = reportDate;
-        this.patientObservation = patientObservation;
-        this.doctorObservation = doctorObservation;
         this.symptoms = symptoms;
         this.signals = signals;
     }
 
-
+    public Report(Patient patient, LocalDate reportDate, String patientObservation, Set<Symptoms> symptoms) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.patientObservation = patientObservation;
+        this.symptoms = symptoms;
+    }
+    public Report(Patient patient, LocalDate reportDate, Set<Symptoms> symptoms, Set<Signal> signals) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.symptoms = symptoms;
+        this.signals = signals;
+    }
+    public Report(Patient patient, LocalDate reportDate, String patientObservation, Set<Signal> signals) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.patientObservation = patientObservation;
+        this.signals = signals;
+    }
+    public Report(Patient patient, LocalDate reportDate, String patientObservation) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.patientObservation = patientObservation;
+    }
+    public Report(Patient patient, LocalDate reportDate, Set<Signal> signals) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.signals = signals;
+    }
+    public Report(Patient patient, LocalDate reportDate, Set<Symptoms> symptoms) {
+        this.patient = patient;
+        this.reportDate = reportDate;
+        this.symptoms = symptoms;
+    }
 }
